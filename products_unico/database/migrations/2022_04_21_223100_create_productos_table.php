a735476efab18bff7a629ea02871231e86dac84c
10carabal <daniel.otero01@usc.edu.co>
2022-04-21 18:07:44 -0500

    create project first approach bd

---
+++ products_unico/database/migrations/2022_04_21_223100_create_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('Nombre', 150);
+            $table->float('Precio');
+            $table->string('Descripcion', 550);
+            $table->string('Imagen', 300)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
